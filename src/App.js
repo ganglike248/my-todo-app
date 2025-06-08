a5d574431cca368ef5426fcc7cc2a38312fe57f4
ganglike <rudfkr248@gmail.com>
2025-06-08 17:46:03 +0900

    프로젝트 초기 설정 완료

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>나의 Todo List</h1>
+      <p>곧 멋진 할 일 목록 앱이 될 예정입니다! 🎉</p>
+    </div>
+  );
+}
+
+export default App;
